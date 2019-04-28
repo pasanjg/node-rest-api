8c6ca5f95454d310aaead06b7b54c70edc22e344
pasanjg <pasanjg@gmail.com>
2019-04-28 21:11:26 +0530

    initial commit - basic routes

---
+++ api/routes/products.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "handling GET requests to /products"
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: "handling POST requests to /products",
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special'){
+        res.status(200).json({
+            message: "You found the special prduct"
+        });
+    }else{
+        res.status(200).json({
+            message: "Id passed ok"
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product'
+    });
+});
+
+module.exports = router;
