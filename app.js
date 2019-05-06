d1bc67521b81f9fef1b618768e8dae47b5358b55
pasanjg <pasanjg@gmail.com>
2019-05-07 02:48:39 +0530

    user login added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect('mongodb://localhost/stockDb', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
